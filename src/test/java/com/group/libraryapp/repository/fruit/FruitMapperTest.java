8b8eb9d95e4b2cc16a0a63c4842b5f9fe7b80ad7
nimosarang <111734043+nimosarang@users.noreply.github.com>
2023-10-01 23:05:37 +0900

    Delete src/test/java/com/group/libraryapp/repository/fruit directory
    repository 리팩토링

--- src/test/java/com/group/libraryapp/repository/fruit/FruitMapperTest.java
+++
@@ -1,9 +0,0 @@
-package com.group.libraryapp.repository.fruit;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.springframework.web.bind.annotation.RestController;
-
-class FruitMapperTest {
-
-}
